cd79d0fff6092e1ea738c494f4cb00d51b6edb3f
Hutchins <kashari.hutchins@accenture.com>
2025-03-26 17:01:50 -0400

    inital commit

---
+++ hello.py
@@ -0,0 +1,314 @@
+ #340.2.1 operations part 1
+
+a = 32  # Initialize the value of a 
+b = 6   # Initialize the value of b
+print('Addition of two numbers:' ,a+b)
+print('Subtraction of two numbers:' ,a-b)
+print('Multiplication of two numbers:' ,a*b)
+print('Division of two nmubers:' ,a/b)
+print('Reminder of two nmubers:' ,a%b)
+print('Exponent of two numbers:' ,a**b)
+print('Floor division of two numbers:' ,a//b)
+MultipleOperators = 1 + 2 * 3 / 4.0
+print("Result of MultipleOperators is: ",MultipleOperators) 
+
+
+c= a**b-7
+print(c)
+
+a=b: False
+a+=b: 38
+a-=b: 26
+a*=b: 192
+a%=b: 2
+a**=b: 1073741824below:
+a//=b: 5
+j is :  10
+j is :  5
+k is :  5
+j is :  10
+k is :  10
+
+
+a = 32      # Initialize the value of a  
+b = 6       # Initialize the value of b  
+print('Two numbers are equal or not:',a==b)  
+print('Two numbers are not equal or not:',a!=b)  
+print('a is less than or equal to b:',a<=b)  
+print('a is greater than or equal to b:',a>=b)  
+print('a is greater b:',a>b)  
+print('a is less than b:',a<b)  
+
+
+Two numbers are equal or not: False
+Two numbers are not equal or not: True
+a is less than or equal to b: False
+a is greater than or equal to b: True
+a is greater b: True
+a is less than b: False
+
+
+a = 5        # initialize the value of a    
+print('Is this statement true?:',a > 3 and a < 5)  
+print('Any one statement is true?:',a > 3 or a < 5)  
+print('Each statement is true then return False and vice-versa:',(not(a > 3 and a < 5)))
+
+a = ["Rose", "Lotus"]
+b = ["Rose", "Lotus"]
+c = a
+print(a is c)
+print(a is not c)
+print(a is b)
+print(a is not b)
+print(a == b)
+print(a != b)
+
+
+x = ["Rose", "Lotus"]  
+print(' Is value Present?', "Rose" in x)  
+print(' Is value not Present?', "Riya" not in x)    
+
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+
+
+Now, we run the above code in Python, and then the output is given below:
+
+
+Is value Present? True
+Is value not Present? True
+True
+True
+True
+False
+
+
+x = ["Rose", "Lotus"]  
+print(' Is value Present?', "Rose" in x)  
+print(' Is value not Present?', "Riya" not in x)    
+
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+
+
+Now, we run the above code in Python, and then the output is given below:
+
+
+Is value Present? True
+Is value not Present? True
+True
+True
+True
+False
+
+
+x = ["Rose", "Lotus"]  
+print(' Is value Present?', "Rose" in x)  
+print(' Is value not Present?', "Riya" not in x)    
+
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+
+
+Now, we run the above code in Python, and then the output is given below:
+
+
+Is value Present? True
+Is value not Present? True
+True
+True
+True
+False
+
+
+x = ["Rose", "Lotus"]  
+print(' Is value Present?', "Rose" in x)  
+print(' Is value not Present?', "Riya" not in x)    
+
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+
+
+Now, we run the above code in Python, and then the output is given below:
+
+
+Is value Present? True
+Is value not Present? True
+True
+True
+True
+False
+
+
+x = ["Rose", "Lotus"]  
+print(' Is value Present?', "Rose" in x)  
+print(' Is value not Present?', "Riya" not in x)    
+
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+
+
+Now, we run the above code in Python, and then the output is given below:
+
+
+Is value Present? True
+Is value not Present? True
+True
+True
+True
+False
+
+
+x = ["Rose", "Lotus"]  
+print(' Is value Present?', "Rose" in x)  
+print(' Is value not Present?', "Riya" not in x)    
+
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+
+
+Now, we run the above code in Python, and then the output is given below:
+
+
+Is value Present? True
+Is value not Present? True
+True
+True
+True
+False
+
+
+x = ["Rose", "Lotus"]  
+print(' Is value Present?', "Rose" in x)  
+print(' Is value not Present?', "Riya" not in x)    
+
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+
+
+Now, we run the above code in Python, and then the output is given below:
+
+
+Is value Present? True
+Is value not Present? True
+True
+True
+True
+False
+
+
+x = ["Rose", "Lotus"]  
+print(' Is value Present?', "Rose" in x)  
+print(' Is value not Present?', "Riya" not in x)    
+
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+# check if '1' key is present in y
+print(1 in y)  # prints True
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+
+
+Now, we run the above code in Python, and then the output is given below:
+
+
+Is value Present? True
+Is value not Present? True
+True
+True
+True
+False
+
+
---
+++ terminal_practice.py
@@ -0,0 +1,12 @@
+import os
+os.name
+os.environ
+
+
+print (os.environ['HOMEDRIVE'])
+x = 42 
+y = -10
+z = 0 
+print(x + y )
+print(x* z) 
+
